d061080886be9732c90f439becb8f3d68aada84c
briecodes <hello@brie.work>
2018-05-03 10:08:38 -0400

    brought over example code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,8 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
+
+  # def post_params
+  #   params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+  # end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,20 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  :post_categories accepts_nested_attributes_for :users
+  # :post_categories accepts_nested_attributes_for :users
+
+  def users_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.user.build(username: username)
+    end
+  end
+
+  # def categories_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     category = Category.find_or_create_by(category_attribute)
+  #     self.post_categories.build(category: category)
+  #   end
+  # end
+
 end
